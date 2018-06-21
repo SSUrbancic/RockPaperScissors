ab31fef3b1c0a17d078b54f28e71cf873b0090da
Sam <SSUrbancic@outlook.com>
2018-06-21 15:08:49 -0500

    created function to manually choose an option for human players.) clear

--- RockPaperScissors/Game.cs
+++ RockPaperScissors/Game.cs
@@ -8,13 +8,19 @@ namespace RockPaperScissors
 {
     class Game
     {
-        string[] gestureArray = { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
 
+
+        string[] gestureArray = { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
+        
+        
+        
+        
+        //Constructor
         public Game()
         {
             DetermineRandomGesture();
-            Console.WriteLine(DetermineRandomGesture());
-
+            DetermineGesture();
+            Console.WriteLine(DetermineGesture());
         }
 
         //method
@@ -23,16 +29,55 @@ namespace RockPaperScissors
             string randomSelection = gestureArray[new Random().Next(0, gestureArray.Length)];
             return randomSelection;
         }
+        
+        public string DetermineGesture()
+        {
+            Console.WriteLine("Choose a gesture: 1- rock, 2 - paper, 3 - scissors, 4 - Lizard, 5 - Spock");
+            string userInput = Console.ReadLine();
+            string gesture;
 
+            if (userInput == "1")
+            {
+                gesture = gestureArray[0];
+                return gesture;
+            }
+            else if (userInput == "2")
+            {
+                gesture = gestureArray[1];
+                return gesture;
+            }
+            else if (userInput == "3")
+            {
+                gesture = gestureArray[2];
+                return gesture;
+            }
+            else if (userInput == "4")
+            {
+                gesture = gestureArray[3];
+                return gesture;
+            }
+            else if (userInput == "5")
+            {
+                gesture = gestureArray[4];
+                return gesture;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+                DetermineGesture();
+                return null;
+            }
+
+
+
+
+
+
+        }
             public void GameFunction()
             {
-                Console.WriteLine("Choose a gesture: 1- rock, 2 - paper, 3 - scissors, 4 - spock, 5 - lizard");
-                string gesture = Console.ReadLine();
 
-                if (gesture == "1")
-                {
 
-                }
             }
 
 
