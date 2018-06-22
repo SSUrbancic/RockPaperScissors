d5a454f187d2b27ae16a40a503404a039b3aece0
Sam <SSUrbancic@outlook.com>
2018-06-21 22:33:14 -0500

    Added a function to compare player input and determine a winner for each round. Also added a Player class and Child classes called Human and Computer.

--- RockPaperScissors/Game.cs
+++ RockPaperScissors/Game.cs
@@ -9,63 +9,67 @@ namespace RockPaperScissors
     class Game
     {
 
+        //gestureArray[0] = Rock, gestureArray[1] = Paper, gestureArray[2] = Scissors, gestureArray[3] = Spock, gestureArray[4] = Lizard// 
+        public int[] gestureArray = { 0, 1, 2, 3, 4 };
+        public int playerOneChoice;
+        public int playerTwoChoice;
 
-        string[] gestureArray = { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
-        
-        
-        
-        
+
+
         //Constructor
         public Game()
         {
-            DetermineRandomGesture();
-            DetermineGesture();
-            Console.WriteLine(DetermineGesture());
+            Console.WriteLine(ExecuteRound());
         }
 
         //method
-        public string DetermineRandomGesture()
+        public int DetermineRandomGesture()
         {
-            string randomSelection = gestureArray[new Random().Next(0, gestureArray.Length)];
+            int randomSelection = gestureArray[new Random().Next(0, gestureArray.Length)];
             return randomSelection;
         }
         
-        public string DetermineGesture()
+        public int DetermineGesture()
         {
-            Console.WriteLine("Choose a gesture: 1- rock, 2 - paper, 3 - scissors, 4 - Lizard, 5 - Spock");
-            string userInput = Console.ReadLine();
-            string gesture;
+            Console.WriteLine("Choose a gesture: 1- rock, 2 - paper, 3 - scissors, 4 - Spock, 5 - Lizard");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int gesture;
 
-            if (userInput == "1")
+            if (userInput == 1)
             {
                 gesture = gestureArray[0];
+                Console.WriteLine("Rock");
                 return gesture;
             }
-            else if (userInput == "2")
+            else if (userInput == 2)
             {
                 gesture = gestureArray[1];
+                Console.WriteLine("Paper");
                 return gesture;
             }
-            else if (userInput == "3")
+            else if (userInput == 3)
             {
                 gesture = gestureArray[2];
+                Console.WriteLine("Scissors");
                 return gesture;
             }
-            else if (userInput == "4")
+            else if (userInput == 4)
             {
                 gesture = gestureArray[3];
+                Console.WriteLine("Spock");
                 return gesture;
             }
-            else if (userInput == "5")
+            else if (userInput == 5)
             {
                 gesture = gestureArray[4];
+                Console.WriteLine("Lizard");
                 return gesture;
             }
             else
             {
-                Console.WriteLine("Invalid Input");
-                DetermineGesture();
-                return null;
+                    Console.WriteLine("Invalid Input, please try again!!!");
+                    DetermineGesture();
+                    return 100;
             }
 
 
@@ -74,14 +78,32 @@ namespace RockPaperScissors
 
 
         }
-            public void GameFunction()
+            public string ExecuteRound()
             {
+            playerOneChoice = DetermineGesture();
+            Console.ReadLine();
+            playerTwoChoice = DetermineRandomGesture();
+            Console.WriteLine(Console.ReadLine());
+            int roundValue = (5 + playerOneChoice - playerTwoChoice) % 5;
 
-
+            if (roundValue == 1 || roundValue == 3)
+            {
+                Console.WriteLine("Player One Wins!");
+                return Console.ReadLine();
+            }
+            else if (roundValue == 2 || roundValue == 4)
+            {
+                Console.WriteLine("Player Two Wins!");
+                return Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+                return Console.ReadLine();
             }
 
+            
 
-
-
-        }
+            }
+    }
     }
